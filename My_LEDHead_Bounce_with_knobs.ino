5a6a2c7d6b555479641ca02de8d94e2586ba4336
smokeandlights <smokeandlights@hotmail.com>
2016-11-11 05:12:48 -0500

    Update My_LEDHead_Bounce_with_knobs.ino

--- My_LEDHead_Bounce_with_knobs.ino
+++ My_LEDHead_Bounce_with_knobs.ino
@@ -1,38 +1,48 @@
 #include <SoftPWM.h>
 
-#define DELAY 100
+
 uint8_t leds[13] = {2,3,4,5,6,7,8,9,10,11,12,A0,A1};
 
 void setup()
 {
-  SoftPWMBegin();
+  int KNOB1 = A2;
+  int KNOB2 = A3;
+  pinMode(KNOB1, INPUT);
+  pinMode(KNOB2,INPUT);
+    SoftPWMBegin();
 
   for (int i = 0; i < 13; i++)
     SoftPWMSet(leds[i], 0);
 
-  SoftPWMSetFadeTime(ALL, 50, 400);
+ 
 }
 
 void loop()
 {
+ 
+  int DELAY = map(KNOB1,0,1023,1,500);
+  int BRIGHTNESS = map(KNOB2,0,1024,1,255);
+
+   SoftPWMSetFadeTime(ALL, (DELAY/8), DELAY);
+  
   int i;
 
   for (i = 0; i < 12; i++)
   {
-    SoftPWMSet(leds[i+1], 255);
+    SoftPWMSet(leds[i+1], BRIGHTNESS);
     SoftPWMSet(leds[i], 0);
     delay(DELAY);
   }
   
-  delay(400);
+  delay(DELAY);
   
   for (i = 12; i > 0; i--)
   {
-    SoftPWMSet(leds[i-1], 255);
+    SoftPWMSet(leds[i-1], BRIGHTNESS);
     SoftPWMSet(leds[i], 0);
     delay(DELAY);
   }
 
-  delay(400);
+  delay(DELAY);
 
 }
