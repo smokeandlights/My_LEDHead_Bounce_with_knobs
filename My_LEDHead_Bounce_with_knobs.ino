8baf91b01ab88b6dce5dbdd8a44ceeb1b72e058d
smokeandlights <smokeandlights@hotmail.com>
2016-11-11 07:59:32 -0500

    Update My_LEDHead_Bounce_with_knobs.ino

--- My_LEDHead_Bounce_with_knobs.ino
+++ My_LEDHead_Bounce_with_knobs.ino
@@ -1,48 +1,46 @@
 #include <SoftPWM.h>
 
-
+//#define DELAY 50
 uint8_t leds[13] = {2,3,4,5,6,7,8,9,10,11,12,A0,A1};
 
 void setup()
 {
-  int KNOB1 = A2;
-  int KNOB2 = A3;
-  pinMode(KNOB1, INPUT);
-  pinMode(KNOB2,INPUT);
-    SoftPWMBegin();
+  SoftPWMBegin();
 
   for (int i = 0; i < 13; i++)
     SoftPWMSet(leds[i], 0);
 
- 
+  
 }
 
 void loop()
 {
- 
-  int DELAY = map(KNOB1,0,1023,1,500);
-  int BRIGHTNESS = map(KNOB2,0,1024,1,255);
-
-   SoftPWMSetFadeTime(ALL, (DELAY/8), DELAY);
-  
+  int KNOB1 = analogRead(A2);
+  int KNOB2 = analogRead(A3);
+  int DELAY = map(KNOB1, 0, 1024, 30, 200);
+  int DELAY2 = map(KNOB1, 0, 1024, 30, 400);
+  int Brightness = map(KNOB2, 0, 1024,1,255);
+  int fadeUP = map(KNOB1,0,1024,10, 100);
+  int fadeDN = map(KNOB1, 0,1024,75,800);
+  SoftPWMSetFadeTime(ALL, fadeUP, fadeDN);
   int i;
 
   for (i = 0; i < 12; i++)
   {
-    SoftPWMSet(leds[i+1], BRIGHTNESS);
+    SoftPWMSet(leds[i+1], Brightness);
     SoftPWMSet(leds[i], 0);
     delay(DELAY);
   }
   
-  delay(DELAY);
+  delay(DELAY2);
   
   for (i = 12; i > 0; i--)
   {
-    SoftPWMSet(leds[i-1], BRIGHTNESS);
+    SoftPWMSet(leds[i-1], Brightness);
     SoftPWMSet(leds[i], 0);
     delay(DELAY);
   }
 
-  delay(DELAY);
+  delay(DELAY2);
 
 }
